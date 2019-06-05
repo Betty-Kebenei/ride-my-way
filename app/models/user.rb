75ac99965bc55dc0ebbb9676a446bdf0cad01f94
Betty-Kebenei <kebeneibetty13@gmail.com>
2019-06-05 16:45:56 +0300

    bg(user) fix saving of user details

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,11 +11,9 @@ class User < ApplicationRecord
 
   def self.from_omniauth(auth)
     where(email: auth.info.email).first_or_initialize do |user|
-      user.first_name = auth.info.given_name
-      user.last_name = auth.info.family_name
+      user.name = auth.info.name
       user.email = auth.info.email
-      user.image_url = auth.info.picture
-      user.google_token = auth.credentials.token
+      user.image_url = auth.info.image
       if user.valid?
         user.save!
       end
